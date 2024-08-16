e260571c455492910ecc0e5950db3175e5c6c904
yang99 <yang.zhang99@outlook.com>
2024-08-16 15:23:42 +0800

    [init]

--- java-study/src/main/java/org/listener/MyRobotListener.java
+++ java-study/src/main/java/org/listener/MyRobotListener.java
@@ -1,16 +1,16 @@
-package test.org.listener;
-
-
-public class MyRobotListener implements RobotListener {
-    @Override
-    public void working(Even even) {
-        Robot robot = even.getRobot();
-        System.out.println("机器人工作提示：请看管好的你机器人，防止它偷懒！");
-    }
-
-    @Override
-    public void dancing(Even even) {
-        Robot robot = even.getRobot();
-        System.out.println("机器人跳舞提示：机器人跳舞动作优美，请不要走神哦！");
-    }
-}
+//package test.org.listener;
+//
+//
+//public class MyRobotListener implements RobotListener {
+//    @Override
+//    public void working(Even even) {
+//        Robot robot = even.getRobot();
+//        System.out.println("机器人工作提示：请看管好的你机器人，防止它偷懒！");
+//    }
+//
+//    @Override
+//    public void dancing(Even even) {
+//        Robot robot = even.getRobot();
+//        System.out.println("机器人跳舞提示：机器人跳舞动作优美，请不要走神哦！");
+//    }
+//}
--- java-study/src/main/java/org/listener/TestListener.java
+++ java-study/src/main/java/org/listener/TestListener.java
@@ -1,14 +1,14 @@
-package test.org.listener;
-
-public class TestListener {
-
-    public static void main(String[] args) {
-        Robot robot = new Robot();
-
-        robot.registerListener(new MyRobotListener());
-
-        robot.working();
-
-        robot.dancing();
-    }
-}
+//package test.org.listener;
+//
+//public class TestListener {
+//
+//    public static void main(String[] args) {
+//        Robot robot = new Robot();
+//
+//        robot.registerListener(new MyRobotListener());
+//
+//        robot.working();
+//
+//        robot.dancing();
+//    }
+//}
